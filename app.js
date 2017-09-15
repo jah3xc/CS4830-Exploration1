fb0b093154d4517206d1a58b6875a146dbf678b2
James Hurt <jah3xc@mail.missouri.edu>
2017-09-14 21:35:51 -0500

    Angular code

---
+++ app.js
@@ -0,0 +1,16 @@
+angular.module('sortApp', [])
+
+.controller('mainController', function($scope) {
+  $scope.sortType     = 'name'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  $scope.search   = '';     // set the default search/filter term
+  
+  // create the list of sushi rolls 
+  $scope.classes = [
+    { name: 'Intro to C', code: "CS1050", professor: "Guilliams" },
+    { name: 'Intro to Java', code: "CS3330", professor: "Wergeles" },
+    { name: 'Operating Systems', code: "CS4530", professor: "Ries" },
+    { name: 'Computational Intelligence', code: "CS47770", professor: "Keller" }
+  ];
+  
+});
